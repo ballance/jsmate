db10e65267716c8cffe767b891d362c8b87473eb
Chris Ballance <ballance@gmail.com>
2016-05-22 11:33:37 -0400

    Refactor chessboard for better encapsulation and fix but in equals operator override

--- JsMate.Service/Models/ChessBoard.cs
+++ JsMate.Service/Models/ChessBoard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -29,53 +30,68 @@ namespace JsMate.Service.Models
         public ChessBoard()
         {
             if (_created) return;
-            // Setting up initial chessboard state
 
+            try
+            {
+                _pieces.AddRange(SetInitialBoardState());
+                _created = true;
+            }
+            catch (Exception)
+            {
+                _created = false;
+            }
+            // If not already create set up initial chessboard state
+           
+        }
+
+        private List<ChessPiece> SetInitialBoardState()
+        {
+            var initialPieces = new List<ChessPiece>();
             // Add Pawns
             for (var i = 0; i < 8; i++)
             {
-                _pieces.Add(new Pawn(PieceTeam.White) { BoardPosition = new BoardPosition(6, i) });
-                _pieces.Add(new Pawn(PieceTeam.Black) { BoardPosition = new BoardPosition(1, i) });
+                initialPieces.Add(new Pawn(PieceTeam.White) {BoardPosition = new BoardPosition(6, i)});
+                initialPieces.Add(new Pawn(PieceTeam.Black) {BoardPosition = new BoardPosition(1, i)});
             }
 
             // Add White Rooks
-            _pieces.Add(new Rook() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 0) });
-            _pieces.Add(new Rook() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 7) });
+            initialPieces.Add(new Rook() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 0)});
+            initialPieces.Add(new Rook() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 7)});
 
             // Add Black Rooks
-            _pieces.Add(new Rook() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 0) });
-            _pieces.Add(new Rook() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 7) });
+            initialPieces.Add(new Rook() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 0)});
+            initialPieces.Add(new Rook() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 7)});
 
             // Add White Knights
-            _pieces.Add(new Knight() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 1) });
-            _pieces.Add(new Knight() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 6) });
+            initialPieces.Add(new Knight() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 1)});
+            initialPieces.Add(new Knight() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 6)});
 
             // Add Black Knights
-            _pieces.Add(new Knight() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 1) });
-            _pieces.Add(new Knight() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 6) });
+            initialPieces.Add(new Knight() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 1)});
+            initialPieces.Add(new Knight() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 6)});
 
 
             // Add White Bishops
-            _pieces.Add(new Bishop() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 2) });
-            _pieces.Add(new Bishop() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 5) });
+            initialPieces.Add(new Bishop() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 2)});
+            initialPieces.Add(new Bishop() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 5)});
 
             // Add Black Bishops
-            _pieces.Add(new Bishop() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 2) });
-            _pieces.Add(new Bishop() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 5) });
+            initialPieces.Add(new Bishop() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 2)});
+            initialPieces.Add(new Bishop() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 5)});
 
             // Add White Queen
-            _pieces.Add(new Queen() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 3) });
+            initialPieces.Add(new Queen() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 3)});
 
             // Add Black Queen
-            _pieces.Add(new Queen() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 3) });
+            initialPieces.Add(new Queen() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 3)});
 
             // Add White King
-            _pieces.Add(new King() { PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 4) });
+            initialPieces.Add(new King() {PieceTeam = PieceTeam.White, BoardPosition = new BoardPosition(7, 4)});
 
             // Add Black King
-            _pieces.Add(new King() { PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 4) });
+            initialPieces.Add(new King() {PieceTeam = PieceTeam.Black, BoardPosition = new BoardPosition(0, 4)});
 
-            _created = true;
+            return initialPieces;
         }
 
         public ChessBoard(List<ChessPiece> pieces)
@@ -98,8 +114,8 @@ namespace JsMate.Service.Models
             }
 
             // TODO: Find a cleaner way to make this comparison.  Goal is to ensure the same pieces are in the same places on both boards.
-            return _pieces.All(piece => chessBoard.Pieces.Contains(piece)); // &&
-                   //chessBoard.Pieces.All(piece2 => _pieces.Contains(piece2));
+            return _pieces.All(piece => chessBoard.Pieces.Contains(piece))  &&
+                   chessBoard.Pieces.All(piece2 => _pieces.Contains(piece2));
 
         }
 
